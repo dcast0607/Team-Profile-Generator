3cdeaffc23919ac7fc486c5895d745a98bc882c5
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-08-28 18:58:02 -0700

    Adding more base files

---
+++ index.js
@@ -0,0 +1,9 @@
+// Bringing in our class definitions from the lib directory. 
+const { Employee, Engineer, Intern, Manager } = require('./lib');
+
+// Brings in the inquirer package so that we can use it.
+const inquirer = require('inquirer');
+
+// Brings in the fs package so that we can use it. 
+const fs = require('fs');
+
