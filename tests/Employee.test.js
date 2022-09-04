0e3827d6ba46e1b30e987ae27a387bf7b6a03409
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-04 00:37:51 -0700

    Pushing comments for employee test file

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -3,25 +3,30 @@
  
 const Employee = require('../lib/Employee');
 
+// We create a tester object here to use as seed data for our testing suite.
 const testerEmployee = {
     name: "Elliot Alderson",
     id: "A123456",
     email: "mrRobot123@mailinator.com"
 }
 
+// Tester object is used to create a new Employee object.
 const EmployeeEntry = new Employee(
     testerEmployee.name, 
     testerEmployee.id, 
     testerEmployee.email
     );
 
-
+// This test will be used to verify that given the employee seed data that we have passed
+// gets added to the appropriate fields and creates a new Employee object successfully.
 test("If employee data creates a new employee record given the right properties", () => {
+    // Here we make sure that the fields match up with the seed data. 
     expect(EmployeeEntry.name).toBe(testerEmployee.name);
     expect(EmployeeEntry.id).toBe(testerEmployee.id);
     expect(EmployeeEntry.email).toBe(testerEmployee.email);
 });
 
+// The tests below will ensure that our functions return the right data. 
 test("If object name is returned by using the getName function", () => {
     expect(EmployeeEntry.getName()).toBe(testerEmployee.name);
 });
